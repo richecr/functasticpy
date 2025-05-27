5b2fe2b5444b4a20741748770a90e520dabc6cfd
Rich Ramalho <richecr1799@gmail.com>
2025-05-27 00:00:29 -0300

    chore: added mypy in project

--- functasticpy/any_pass.py
+++ functasticpy/any_pass.py
@@ -5,9 +5,7 @@ from functasticpy.curry import curry
 T = TypeVar("T")
 
 
-def any_pass(
-    *args: Union[T, List[Callable[[T], bool]]],
-) -> Union[bool, Callable[[T], bool]]:
+def any_pass(*args: Union[T, List[Callable[[T], bool]]]) -> Union[bool, Callable[[T], bool]]:
     def any_pass_implementation(data: T, fns: List[Callable[[T], bool]]) -> bool:
         return any(fn(data) for fn in fns)
 
